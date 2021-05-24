a3015bbb9ac5bf253adf5f7ab436216a796d4f9a
jarred <jarred.caris@gmail.com>
2021-05-24 21:01:07 +0930

    set up for mongoDB connection but cant get connection, will return later to rectify

--- server/index.js
+++ server/index.js
@@ -2,33 +2,24 @@ import express from "express";
 import mongoose from "mongoose";
 import cors from "cors";
 
-
-import postRoutes from './routes/posts.js'
-
-const PORT = process.env.PORT || 3001;
+import postRoutes from "./routes/posts.js";
 
 const app = express();
-
-
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cors());
 
-app.use('/posts', postRoutes)
+app.use("/posts", postRoutes);
 
-app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/social", {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  useCreateIndex: true,
-  useFindAndModify: false
-});
+const CONNECTION_URL =
+  "mongodb+srv://jarred86:password1234@cluster0.musdc.mongodb.net/dbname?retryWrites=true&w=majority";
 
-// routes
-// app.use(routes);
+const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
-  console.log(`App running on port ${PORT}!`);
-});
+mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => app.listen(PORT, () => console.log(`Server Running on Port: http://localhost:${PORT}`)))
+  .catch((error) => console.log(`${error} did not connect`));
+
+  mongoose.set('useFindAndModify', false);

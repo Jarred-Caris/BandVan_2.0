de64f15d271a8e6d2fce90e62a20141e509b643c
jarred <jarred.caris@gmail.com>
2021-05-27 09:17:54 +0930

    masked connection url with dotenv

--- server/index.js
+++ server/index.js
@@ -1,10 +1,11 @@
 import express from "express";
 import mongoose from "mongoose";
 import cors from "cors";
-
+import dotenv from 'dotenv'
 import postRoutes from "./routes/posts.js";
 
 const app = express();
+dotenv.config()
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -12,12 +13,10 @@ app.use(cors());
 
 app.use("/posts", postRoutes);
 
-const CONNECTION_URL =
-  "mongodb+srv://JarredC86:Pablonala_86@cluster0.musdc.mongodb.net/dbname?retryWrites=true&w=majority";
 
 const PORT = process.env.PORT || 5000;
 
-mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(process.env.CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() =>
     app.listen(PORT, () =>
       console.log(`Server Running on Port: http://localhost:${PORT}`)
